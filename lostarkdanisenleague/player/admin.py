69c30ed5dd56e0854458eafa1f382cbde4ea801a
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-07-27 22:08:21 -0400

    Added basic db layout.

---
+++ lostarkdanisenleague/player/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from django.db import models
+
+from django.contrib.auth.models import models, User
+
+# Register your models here.
+
+
+class Player(models.Model):
+    user_id = models.OneToOneField(User, null=True)
+
+
+class Fighter(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Record(models.Model):
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    ties = models.IntegerField(default=0)
+    game = models.ForeignKey(Game)
+    fighter_id = models.ForeignKey(Fighter)
+    player_id = models.ForeignKey(Player)
