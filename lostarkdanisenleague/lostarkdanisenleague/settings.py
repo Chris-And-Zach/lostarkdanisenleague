77357b3d6c3af82d7991f1bca432a409575d5f14
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-07-27 21:18:13 -0400

    Added database information to settings.py.

--- lostarkdanisenleague/lostarkdanisenleague/settings.py
+++ lostarkdanisenleague/lostarkdanisenleague/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'lostarkdanisenleague.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psychopg2',
+        'NAME': 'danisen_league_db',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
