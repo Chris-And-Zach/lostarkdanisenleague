d1a2a28be2efc262309ee0a32ad41cec5dfb63f2
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-07-27 22:14:32 -0400

    Fixed moving models from admin to player.

--- lostarkdanisenleague/player/models.py
+++ lostarkdanisenleague/player/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from django.contrib.auth.models import models, User
 # Create your models here.
+
+class Player(models.Model):
+    user_id = models.OneToOneField(User, null=True)
+
+
+class Fighter(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Record(models.Model):
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    ties = models.IntegerField(default=0)
+    game = models.ForeignKey(Game)
+    fighter_id = models.ForeignKey(Fighter)
+    player_id = models.ForeignKey(Player)
