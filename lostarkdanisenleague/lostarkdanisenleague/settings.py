5c086fa5916d276bac007bb675a12dcbc1861d14
Zach <zach>
2015-08-08 17:17:36 -0400

    Added the CSS and made it so that postgres is running properly.

--- lostarkdanisenleague/lostarkdanisenleague/settings.py
+++ lostarkdanisenleague/lostarkdanisenleague/settings.py
@@ -78,8 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'danisen_league_db',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
